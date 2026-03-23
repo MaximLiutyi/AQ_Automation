929a8a091ca2469c6d595b01fb2730c0a5b74c5c
Maks <maks.zloy@ukr.net>
2026-03-23 21:18:12 +0200

    xpass helper

---
+++ Lesson_7/xpass.py
@@ -0,0 +1,5 @@
+# how to find elements
+
+# //input[@type='email']   -  шукаємо в інпуті тайп мейл
+# //button[text()='TEXT']  -  якщо шукати по тексту @ не буде працювати треба ()
+# //button[contains(@class, 'l6rDnh6R')] - шукати по класу
